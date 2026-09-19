f7ebfb2bb72d62e5386ee86a7cff5701314e10fd
kenzuko <kenzuko@gmail.com>
2026-09-19 14:15:47 +0700

    art: keep approved pixel masters crisp at runtime

--- src/scenes/AirportScene.ts
+++ src/scenes/AirportScene.ts
@@ -11,7 +11,7 @@ export class AirportScene extends Phaser.Scene {
     const { width, height } = this.scale;
     this.cameras.main.setBackgroundColor('#0a4770');
     const master = this.add.image(width / 2, height / 2, 'master-airport').setDisplaySize(width, height).setDepth(0);
-    master.texture.setFilter(Phaser.Textures.FilterMode.LINEAR);
+    master.texture.setFilter(Phaser.Textures.FilterMode.NEAREST);
     const continueZone = this.add.zone(width / 2, 830, 370, 105).setInteractive({ useHandCursor: true }).setDepth(10);
     continueZone.on('pointerdown', () => flowController.go(this, SceneKeys.Character));
     this.cameras.main.fadeIn(220, 7, 36, 58);
